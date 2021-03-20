8f87c8d8aa48393d9ac7166bc9fb999155b68503
egor <likeroge@gmail.com>
2021-03-20 17:17:23 +0400

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
+});
