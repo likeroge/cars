fff0c1844936c57bbb051eff30af116fbc68f984
egor <likeroge@gmail.com>
2021-03-20 17:19:43 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Cars App</h1>
+        <h2>Welcome</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
